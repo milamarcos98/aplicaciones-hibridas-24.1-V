17d4124ac9cec7e714ba6eda08af4c7f3abe26f8
mila <milamarcos98@gmail.com>
2024-04-21 23:27:30 -0300

    clase 10

---
+++ clase 10/controllers/usuarios_controller.js
@@ -0,0 +1,33 @@
+import Usuario from "../models/usuarios_model.js"
+
+async function getUsers(){
+    let usuarios = await Usuario.find();
+    // let usuarios = await Usuario.find({estado: true});
+    return usuarios;
+}
+
+async function createUser(body){
+   let user = new Usuario({
+        nombre: body.nombre,
+        password: body.password,
+        email: body.email,
+        estado: true
+   })
+   return await user.save();
+}
+
+async function updateUser(body, email){
+    console.log(body)
+    console.log(email)
+    let user = new Usuario.updateOne({"email":email}, {
+        $set:{
+        nombre: body.nombre,
+        password: body.password
+       }
+   })
+    
+    return user;
+ }
+
+
+export { getUsers, createUser, updateUser}
